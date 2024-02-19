ef4fecaa964965c66b6bd7d010f223e1238c3fa5
wiederholung <wiederholung@outlook.com>
2024-02-19 15:19:39 +0800

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Website for the Workshop on "Environment Dynamics Matters: Embodied Navigation to Movable Objects" at IROS 2024
---
+++ files/script.js
@@ -0,0 +1,39 @@
+function toggleAnswer(id) {
+  var answer = document.getElementById(id);
+  if (answer.style.display === "block") {
+    answer.style.display = "none";
+  } else {
+    answer.style.display = "block";
+  }
+}
+
+// The deadline is March 11 End of Day AoE, which is 12 hours behind UCT
+var countDownDate = new Date("Mar 12, 2024 11:59:59").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function () {
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
+
+  // Calculating the days, hours, minutes and seconds left
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  // Result is output to the specific element
+  document.getElementById("days").innerHTML = days + "d ";
+  document.getElementById("hours").innerHTML = hours + "h ";
+  document.getElementById("mins").innerHTML = minutes + "m ";
+  document.getElementById("secs").innerHTML = seconds + "s ";
+
+  // Display the message when countdown is over
+  if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("days").innerHTML = "";
+    document.getElementById("hours").innerHTML = "";
+    document.getElementById("mins").innerHTML = "";
+    document.getElementById("secs").innerHTML = "";
+    document.getElementById("end").innerHTML = "Deadline has passed!";
+  }
+}, 1000);
